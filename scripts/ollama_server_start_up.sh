e87439e25b696195f031faff2882fbe556078b9e
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2025-11-23 22:28:25 +0000

    feat(scripts): initial adhoc commands for installing ollama server
    include manual install steps for ollama server

---
+++ scripts/ollama_server_start_up.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Manual install of Ollama
+# https://docs.ollama.com/linux#manual-install
+curl -fsSL https://ollama.com/install.sh | sh
+
+# Change host from localhost to 0.0.0.0
+# https://github.com/koalaman/shellcheck/wiki/SC2129
+sudo bash -c '{
+  echo "[Service]";
+  echo "Environment=\"OLLAMA_HOST=0.0.0.0\"";
+  echo "Environment=\"OLLAMA_PORT=11434\"";
+} >> /etc/systemd/system/ollama.service'
+
+# Reload systemd, enable and start ollama server 
+sudo systemctl stop ollama && \
+sudo systemctl daemon-reload && \
+sudo systemctl enable ollama && \
+sudo systemctl start ollama
+
+# Pull model needed
+ollama pull gemma3n:e4b
